c3106436390393c56cd7ab4e07491b2557e191de
Александр Махалов <aleksandrmahalov@MacBook-Aleksandr.local>
2019-04-11 22:14:25 +0300

    add

--- src/js/Tabel.js
+++ src/js/Tabel.js
@@ -1,48 +1,48 @@
 class Table {
-    constructor(el, data, time) {
-        this.el = document.querySelector(el);
-        this.data = data;
-        this.time = time;
-        this.counter = 0;
+  constructor(el, data, time) {
+    this.el = document.querySelector(el);
+    this.data = data;
+    this.time = time;
+    this.counter = 0;
 
-        this.init();
-    }
+    this.init();
+  }
 
-    init() {
-        this.createRow(this.data);
-        this.sortByInterval();
-    }
+  init() {
+    this.createRow(this.data);
+    this.sortByInterval();
+  }
 
-    sortByInterval() {
-        setInterval(() => {
-            let i = 0;
-            this.data = this.data.sort((a, b) => {
-                if(i <= this.counter) {
-                    i++;
-                    return a.id - b.id;
-                }
-            });
-            this.el.innerHTML = '';
-            this.createRow(this.data);
-            this.counter++;
-        }, this.time);
-    }
-
-    createRow (rows) {
-        for (const row of rows) {
-            const tr = document.createElement('tr');
-
-            tr.innerHTML = `<td>#${row.id}</td><td>${row.title}</td><td>(${row.year})</td><td>imdb: ${row.imdb}</td>`;
-
-            tr.className = 'js-table-tr';
-            tr.setAttribute('data-id', row.id);
-            tr.setAttribute('data-title', row.title);
-            tr.setAttribute('data-year', row.year);
-            tr.setAttribute('data-imdb', row.imdb);
-
-            this.el.appendChild(tr);
+  sortByInterval() {
+    setInterval(() => {
+      let i = 0;
+      this.data = this.data.sort((a, b) => {
+        if (i <= this.counter) {
+          i++;
+          return a.id - b.id;
         }
+      });
+      this.el.innerHTML = '';
+      this.createRow(this.data);
+      this.counter++;
+    }, this.time);
+  }
+
+  createRow(rows) {
+    for (const row of rows) {
+      const tr = document.createElement('tr');
+
+      tr.innerHTML = `<td>#${row.id}</td><td>${row.title}</td><td>(${row.year})</td><td>imdb: ${row.imdb}</td>`;
+
+      tr.className = 'js-table-tr';
+      tr.setAttribute('data-id', row.id);
+      tr.setAttribute('data-title', row.title);
+      tr.setAttribute('data-year', row.year);
+      tr.setAttribute('data-imdb', row.imdb);
+
+      this.el.appendChild(tr);
     }
+  }
 }
 
 export default Table;
